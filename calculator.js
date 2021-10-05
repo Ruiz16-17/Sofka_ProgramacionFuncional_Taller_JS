ae86e8d2ad9f0887ad3630f5bf43412afb155f11
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-10-05 16:18:52 -0500

    Interface aggregated and functions has been completed.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,6 @@
 const accumulator = Object.freeze(0);
-let option = prompt("¿Qué deseas hacer?",
-    "1. Sumar",
-    "2. Restar",
-    "3. Multiplicar",
-    "4. Dividir");
+let number1, number2;
+
 
 const add = (number1, number2) => {
     return number1 + number2;
@@ -22,7 +19,6 @@ const multiplyPositive = (number1, number2, acumulador) => {
         return multiplyPositive(number1, number2 - 1, acumulador = add(acumulador, number1));
     
 }
-
 
 const multiplyNegative = (number1, number2, acumulador) => {
 
@@ -45,4 +41,53 @@ const divide = (number1, number2, quotient) => {
 
 } 
 
-console.log(divide(12,0,accumulator));
+const verifyMultiplication = (number1, number2) =>{
+    if(number2 < 0){
+        alert("Resultado de la multiplicación de " + number1 + " x " + number2 + " es igual a: " + multiplyNegative(number1,number2,accumulator));
+    }
+    if(number2 > 0){
+        alert("Resultado de la multiplicación de " + number1 + " x " + number2 + " es igual a: " + multiplyPositive(number1,number2,accumulator));
+    }
+    if(number2 == 0 && number1 == 0){
+        alert("Resultado: 0");
+    }
+}
+
+let option = prompt("¿Qué deseas hacer?\n"+
+    "1. Sumar\n"+
+    "2. Restar\n"+
+    "3. Multiplicar\n"+
+    "4. Dividir");
+
+    switch (option) {
+        case "1":
+            number1 = parseInt(prompt("Ingrese el numero 1."));
+            number2 = parseInt(prompt("Ingrese el numero 2."));
+            alert("Resultado de la suma de " + number1 + " + " + number2 + " es igual a: " + add(number1,number2));
+            break;
+        case "2":
+            number1 = prompt("Ingrese el numero 1.");
+            number2 = prompt("Ingrese el numero 2.");
+            alert("Resultado de la resta de " + number1 + " - " + number2 + " es igual a: " + subtract(number1,number2));
+            break;
+            
+        case "3":
+            number1 = parseInt(prompt("Ingrese el numero 1."));
+            number2 = parseInt(prompt("Ingrese el numero 2."));
+            verifyMultiplication(number1, number2);
+            break;
+            
+        case "4":
+            number1 = parseInt(prompt("Ingrese el numero 1."));
+            number2 = parseInt(prompt("Ingrese el numero 2."));
+            alert("Resultado de la división de " + number1 + " / " + number2 + " es igual a: " + divide(number1,number2, accumulator));
+            break;
+
+        case "5":
+            alert("Terminando proceso.");
+            break;
+
+        default:
+            alert("Valor no encontrado.");
+            break;
+    }
